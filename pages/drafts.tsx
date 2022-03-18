d014c8d608e27d4bedcefcab5938028b21b6c1fd
uxiun <teppeiito0905@gmail.com>
2022-03-18 19:10:31 +0900

    drafts検索の一致条件をemailからnameに変更

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -14,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
   const drafts = await prisma.post.findMany({
     where: {
-      author: { email: session.user.email },
+      author: { name: session.user.name },
       published: false,
     },
     include: {
